ed9a4d4121cecb2f3fd01b68f3d38b8ec742ab19
Adrian Ordorica <aordoric@uark.edu>
2014-12-09 16:37:56 -0600

    FIXED IT
    OMG took me forever and it was a PHP echo and a few css stuff but now it
    looks tons better

--- frontend/first.php
+++ frontend/first.php
@@ -51,6 +51,13 @@ if(!isset($_SESSION['username'])){
 	#search {
 		position: aboslute;
 	}
+    body {
+    padding-top: 100px; /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+}
+
+footer {
+    margin: 50px 0;
+}
 	</style>
   </head>
 <!-- NAVBAR
@@ -68,12 +75,12 @@ if(!isset($_SESSION['username'])){
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">BiblioBase</a>
+              <a class="navbar-brand" href="index.html">BiblioBase</a>
             </div>
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
                 <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="about.html">About</a></li>
 				<?php 
 				//only show My Desk if person is logged in
 				if($_SESSION['security'] >= "1"){	
@@ -130,11 +137,127 @@ if(!isset($_SESSION['username'])){
         </div>
       </div>
     </div>
-	<div id="backg">
-		<br><br><br><br><br><br>
-	</div>
-	<div id="homepage">
-		<p>This is where your favorites and such will be. This is also where the last <br> things you checked out will be if you are a patron.</p>
-	</div>
-  </body>
+
+
+	<!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <h1 class="page-header">
+                    My Desk
+                    <small>Welcome back <?php echo $_SESSION['username'];?></small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#">Favorites</a>
+                </h2>
+                <p class="lead">
+                    by <?php echo $_SESSION['username'];?>
+                </p>
+                <!-- Timestamp code we can use later if needed
+                <p><span class="glyphicon glyphicon-time"></span> Posted on December 8, 2014 at 10:00 PM</p>
+                -->
+                <hr>
+                <img class="img-responsive" src="http://samkillermann.com/wp-content/uploads/2014/07/desk-cacti.jpg" alt="">
+                <hr>
+                <p>Tables and things checked out goes here.</p>
+
+                <hr>
+
+                <!-- If we wanted to add more pages, we can add it here. Pager function.
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Account Info</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newest books &rarr;</a>
+                    </li>
+                </ul>
+                -->
+
+            </div>
+
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Search Well -->
+                <div class="well">
+                    <h4>Search</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Components Well -->
+                <div class="well">
+                    <h4>Components</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Books</a>
+                                </li>
+                                <li><a href="#">Movies</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Music</a>
+                                </li>
+                                <li><a href="#">Events</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+                <div class="well">
+                    <h4>Welcome!</h4>
+                    <p>Bibliobase is a new, convenient way to search and track your favorite books, music, movies, and more at your local library! Use the search function above to get started.</p>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Bibliobase 2014</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="view/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="view/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
